c16223550e58cbae48ade3bf232effd45522d0ae
Tushar Mathur <tusharmath@gmail.com>
2016-04-28 19:41:23 +0530

    style(playlist): indentation changes

--- src/ui/playlist/index.js
+++ src/ui/playlist/index.js
@@ -15,10 +15,9 @@ export default ({tracks$, DOM, audio}) => {
   const loadstart$ = audio.events('loadstart')
   const isPlaying$ = Observable.merge(play$.map(true), pause$.map(false), loadstart$.map(false)).startWith(false)
   const playlistItem$ = tracks$
-    .map(tracks => {
-      return tracks
-        .map((track, i) => isolate(PlayListItem, track.id.toString())({track, DOM, trackListClick$, isPlaying$}, i))
-    })
+    .map(tracks => tracks.map((track, i) =>
+      isolate(PlayListItem, track.id.toString())({track, DOM, trackListClick$, isPlaying$}, i)
+    ))
   const playlistItemVTree$ = playlistItem$.map(tracks => tracks.map(x => x.DOM))
   const playlistItemClick$ = playlistItem$.map(tracks => tracks.map(x => x.click$))
 
