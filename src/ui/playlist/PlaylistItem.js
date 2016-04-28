063312c3a9e8cfa7e9a4a8a6cdb401d296c01565
Tushar Mathur <tusharmath@gmail.com>
2016-04-28 13:51:33 +0530

    style(ui): style updates

--- src/ui/playlist/PlaylistItem.js
+++ src/ui/playlist/PlaylistItem.js
@@ -2,12 +2,11 @@
  * Created by imamudin.naseem on 27/04/16.
  */
 
-import {div} from '@cycle/dom'
+import {div, span} from '@cycle/dom'
 import {Observable} from 'rx'
 import * as F from '../../Utils/Flexbox'
 import * as SC from '../../Utils/SoundCloud'
-import * as D from '../../Utils/DOMUtils'
-import {overflowEllipsis, size} from '../../Utils/StyleUtils'
+import {overflowEllipsisSTY, size, subtitleSTY} from '../../Utils/StyleUtils'
 
 const Artwork = url => div({
   style: {
@@ -15,31 +14,47 @@ const Artwork = url => div({
     backgroundImage: url ? `url('${url}')` : null,
     backgroundColor: 'rgb(42, 44, 49)',
     backgroundSize: '35px 35px',
-    backgroundRepeat: 'no-repeat',
-    marginRight: '10px'
+    backgroundRepeat: 'no-repeat'
   }
 })
+const Index = index => div({style: {...subtitleSTY, width: '35px', textAlign: 'center'}}, [index + 1])
 
 const TrackDuration = duration =>
-  div({style: {marginLeft: '1em', color: '#555', fontSize: '0.8em'}}, SC.durationFormat(duration))
+  div({style: {color: '#555', fontSize: '0.8em', padding: '0 10px'}}, SC.durationFormat(duration))
 
 const TrackDetail = ({artist, title}) =>
-  div({style: {overflow: 'hidden', flex: '1 0 0'}}, [
-    div({style: overflowEllipsis}, title),
-    div({style: {color: '#555', fontSize: '0.8em', ...overflowEllipsis}}, artist)
+  div({style: {overflow: 'hidden'}}, [
+    div({style: overflowEllipsisSTY}, title),
+    div({style: {...subtitleSTY, ...overflowEllipsisSTY}}, artist)
   ])
 
-export const PlayListItem = ({DOM, track: {title, user, duration, artwork_url, id}, trackListClick$}) => {
+const playListItemSTY = {
+  fontSize: '1em',
+  fontWeight: 600,
+  alignItems: 'center',
+  ...F.RowSpaceBetween
+}
+
+export default ({DOM, track: {title, user, duration, artwork_url, id}, trackListClick$}, index) => {
   const click$ = DOM.select('.playlist-item').events('click').map(id)
-  const defaultStyle = {fontSize: '1em', fontWeight: 600, padding: '5px 10px', alignItems: 'center', ...F.RowSpaceBetween}
   return {
     click$,
     DOM: Observable.just(div({
-      className: 'playlist-item', style: defaultStyle
+      className: 'playlist-item', style: playListItemSTY
     }, [
-      Artwork(artwork_url),
-      TrackDetail({title, artist: user.username}),
-      TrackDuration(duration)
+      Index(index),
+      div({
+        style: {
+          borderBottom: 'solid 1px rgba(255, 255, 255, 0.1)',
+          padding: '10px 0',
+          flex: '1 0 0',
+          overflow: 'hidden', ...F.RowSpaceBetween,
+          alignItems: 'center'
+        }
+      }, [
+        TrackDetail({title, artist: user.username}),
+        TrackDuration(duration)
+      ])
     ]))
   }
 }
