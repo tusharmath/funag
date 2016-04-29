c9437192b308bec79d02f1726a8cd449609f1ce3
Imam <naseem1992@gmail.com>
2016-04-29 15:31:38 +0530

    feat(theme): adding theme

---
+++ src/Utils/Themes.js
@@ -0,0 +1,14 @@
+export const colors = {
+  'dark-blue': '#314290',
+  'blue': '#4a71c0',
+  'red': '#ab3a2c',
+  'light-red': '#bd5630',
+  'orange': '#e5ad4f',
+  'brown': '#43363a',
+  'green-1': '#6d8325',
+  'green-2': '#a0a584',
+  'green-3': '#697153',
+  'khakhi': '#a88b4c',
+  'light-khakhi': '#c8b272'
+}
+export const theme = colors.blue
