ae6547873504feab0394a416cdfc4c0f5a637bbe
Tushar Mathur <tusharmath@gmail.com>
2016-04-30 15:04:30 +0530

    fix(loader): use rotational loader instead of the expanding one

--- src/ui/controls/PlaybackButtons.js
+++ src/ui/controls/PlaybackButtons.js
@@ -5,15 +5,7 @@
 'use strict'
 import {div, i} from '@cycle/dom'
 import {Observable} from 'rx'
-import * as F from '../../Utils/Flexbox'
 import * as S from '../../Utils/StyleUtils'
-import BallScaleRipple from './BallScaleRipple'
-
-const controlsSTY = {
-  ...F.RowSpaceAround,
-  alignItems: 'center',
-  width: '100%'
-}
 
 export default ({audio, DOM}) => {
   const playPause$ = Observable.merge(
@@ -22,7 +14,7 @@ export default ({audio, DOM}) => {
   ).startWith('play')
     .map(button => div({className: `ctrl-${button}`, style: S.block(50)}, [S.fa(button)]))
 
-  const loadStart$ = audio.events('loadstart').map(div({style: S.block(50)}, [BallScaleRipple(2)]))
+  const loadStart$ = audio.events('loadstart').map(div({style: S.block(50)}, [div('.loader')]))
 
   const audio$ = Observable.merge(
     DOM.select('.ctrl-play').events('click').map({type: 'PLAY'}),
