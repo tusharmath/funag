f1f5b98fc465171efef9a6ab2544de06885515a3
Imam <naseem1992@gmail.com>
2016-05-03 19:13:57 +0530

    fix(component): removing logic from soundVisualiztion
    closes #26

--- src/ui/playlist/SoundVisualizerIcon.js
+++ src/ui/playlist/SoundVisualizerIcon.js
@@ -7,16 +7,13 @@
 import {ul, li, div} from '@cycle/dom'
 import * as S from '../../Utils/StyleUtils'
 
-export default (isAnimating) => {
-  const pauseAnimaton = isAnimating ? null : '.pause-animation'
-  return div({
-    className: 'fade-in',
-    style: {
-      ...S.absolute(),
-      ...S.block(50),
-      backgroundColor: 'rgba(255, 255, 255, 0.8)'
-    }
-  }, [ul('.playing-animation', {style: {...S.size(17)}}, [
-    li(pauseAnimaton), li(pauseAnimaton), li(pauseAnimaton)
-  ])])
-}
+export default div({
+  className: 'fade-in',
+  style: {
+    ...S.absolute(),
+    ...S.block(50),
+    backgroundColor: 'rgba(255, 255, 255, 0.8)'
+  }
+}, [ul('.playing-animation', {style: {...S.size(17)}}, [
+  li(), li(), li()
+])])
