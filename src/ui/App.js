357f23af847dd7a489fb6d4c56da1e3a79f85abc
Tushar Mathur <tusharmath@gmail.com>
2016-05-03 15:42:36 +0530

    fix(search): use flatMapLatest to show results of the latest request that was made
    request should ideally be cancelled by with flatMapLatest the user will always receive the response
    of the latest result thus maintaining the order.

--- src/ui/App.js
+++ src/ui/App.js
@@ -14,7 +14,7 @@ import * as SC from '../Utils/SoundCloud'
 
 export default function ({DOM, route, audio}) {
   const searchBox = SearchBox({DOM, route})
-  const tracks$ = SC.searchTracks(searchBox.value$)
+  const tracks$ = SC.searchTracks(searchBox.value$.debounce(300))
   const playlist = Playlist({tracks$, DOM, audio})
   const selectedTrack$ = playlist.selectedTrack$
 
