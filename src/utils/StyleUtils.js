968aa5cbbcfa9a0494bf191a30563b307836ecd1
Tushar Mathur <tusharmath@gmail.com>
2016-05-14 13:25:46 +0530

    fix(utils): skip null coords in StyleUtils.absolute
    invalid stringifying of coords were getting created

--- src/utils/StyleUtils.js
+++ src/utils/StyleUtils.js
@@ -9,10 +9,16 @@ import * as F from './Flexbox'
 import * as T from './Themes'
 
 export const size = (height, width = height) => ({height: `${height}px`, width: `${width}px`})
-export const absolute = (left = 0, top = 0, right = 0, bottom = 0) => ({
-  top: `${top}px`, bottom: `${bottom}px`, left: `${left}px`, right: `${right}px`,
-  position: 'absolute'
-})
+export const absolute = (left = 0, top = 0, right = 0, bottom = 0) => {
+  const positions = {}
+  const coords = {left, top, right, bottom}
+  Object.keys(coords)
+    .filter(x => coords[x] !== null)
+    .forEach(key => {
+      positions[key] = coords[key]
+    })
+  return {...positions, position: 'absolute'}
+}
 export const fa = (name, dim = 1) => i(`.fa.fa-${name}`, {
   style: {
     fontSize: `${dim}em`,
