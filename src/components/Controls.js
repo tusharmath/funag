7c6433c6a0b069d7b536f73a819648a2aac34858
Imam <naseem1992@gmail.com>
2016-05-17 12:32:38 +0530

    fix(ui): control show be visible only if there is any song selected
    closes #74

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -9,25 +9,31 @@ import Scrobber from './Scrobber'
 import Playback from './Playback'
 import * as S from '../utils/StyleUtils'
 
-const ControlSTY = {
+const ControlSTY = show => ({
   ...S.position({bottom: 0, left: 0, right: 0}),
-  position: 'fixed'
-}
+  position: 'fixed',
+  transform: show ? 'translateY(0%)' : 'translateY(100%)',
+  transition: 'transform ease-in 300ms'
+})
 export default ({audio, selectedTrack$, DOM}) => {
   const completion$ = audio.events('timeupdate').map(x => x.currentTime / x.duration).startWith(0)
   const playback = Playback({selectedTrack$, audio, DOM})
   return {
     audio$: playback.audio$,
-    DOM: Observable.combineLatest(
-      Scrobber({completion$}).DOM,
-      playback.DOM
-    ).map(views =>
-        div({
-          style: {
-            boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.5)',
-            backgroundColor: 'rgb(246, 246, 246)'
-          }
-        }, views))
-      .map(view => div({style: ControlSTY}, [view]))
+    DOM: Observable
+      .combineLatest(
+        selectedTrack$.startWith(null),
+        Scrobber({completion$}).DOM,
+        playback.DOM
+      )
+      .map(([selected, scrobber, playback]) =>
+        div({style: ControlSTY(Boolean(selected))}, [
+          div({
+            style: {
+              boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.5)',
+              backgroundColor: 'rgb(246, 246, 246)'
+            }
+          }, [scrobber, playback])])
+      )
   }
 }
