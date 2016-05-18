4b8bd648c9344a9ef90a94f35e0bba22d482bde9
Imam <naseem1992@gmail.com>
2016-05-18 18:12:41 +0530

    fixup! feat(ui): Expanded version of controls page.

--- src/bootstrapDOM.js
+++ src/bootstrapDOM.js
@@ -33,5 +33,5 @@ Cycle.run(Main, {
   events: eventSinkDriver,
   title: documentTitleDriver,
   HTTP: makeHTTPDriver(),
-  MODEL: makeModelDriver({isServer: false, controlBottom: 0})
+  MODEL: makeModelDriver({isServer: false})
 })
