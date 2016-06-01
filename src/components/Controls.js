94601d12a4c8150f923c0d2896687276c5d4df31
Tushar Mathur <tusharmath@gmail.com>
2016-06-01 11:19:40 +0530

    feat(scrobber): add drag support

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -48,9 +48,10 @@ const model = ({audio$, selectedTrack$}) => {
 export default ({audio$, selectedTrack$, DOM, AUDIO}) => {
   const {completion$, showControls$} = model({audio$, selectedTrack$})
   const playback = Playback({selectedTrack$, audio$, DOM, AUDIO})
-  const scrobber = Scrobber({completion$})
+  const scrobber = Scrobber({completion$, DOM})
   return {
     audio$: playback.audio$,
-    DOM: view({playback, scrobber, showControls$})
+    DOM: view({playback, scrobber, showControls$}),
+    event$: scrobber.event$
   }
 }
--- src/components/Scrobber.js
+++ src/components/Scrobber.js
@@ -4,27 +4,31 @@
 
 'use strict'
 import {div} from 'cycle-snabbdom'
+import R from 'ramda'
+import {Observable as O} from 'rx'
+import {mux} from 'muxer'
 import * as S from '../utils/StyleUtils'
 import * as F from '../utils/Flexbox'
 import {Pallete} from '../utils/Themes'
+import RAFThrottle from '../utils/RAFThrottle'
 
-const view = ({completion$}) => {
-  return completion$.map(completion =>
+const view = ({transition$, translate$}) => {
+  return O.combineLatest(transition$, translate$).map(([transition, translate]) =>
     div([
       div({style: {height: '4px', width: '100%'}}, [
-        div({
+        div('.scrobber-container', {
           style: {
+            transition: transition ? 'transform 100ms linear' : null,
+            transform: `translateX(${translate})`,
+            willChange: 'transform',
             ...F.RowRight,
-            transition: 'transform 100ms linear',
             background: Pallete.primaryDarkColor,
             height: '100%',
-            willChange: 'transform',
-            transform: `translateX(${100 * completion - 100}%)`,
             transformOrigin: 'left',
             marginRight: '15px'
           }
         }, [
-          div({
+          div('.scrobber', {
             style: {
               ...{...S.block(15), borderRadius: '20px'},
               backgroundColor: Pallete.accentColor,
@@ -34,13 +38,43 @@ const view = ({completion$}) => {
           })
         ])
       ])
-    ])
-  )
+    ]))
 }
 
-export default ({completion$}) => {
-  const vTree$ = view({completion$})
+const width = x => R.head(x).getBoundingClientRect().width
+
+const intent = ({DOM}) => {
+  const scrobberEL = DOM.select('.scrobber')
+  const scrobberContainerEL = DOM.select('.scrobber-container')
+  const container$ = scrobberContainerEL.observable
+  const touchStart$ = scrobberEL.events('touchstart')
+  const touchEnd$ = scrobberEL.events('touchend')
+  const touchMove$ = scrobberEL.events('touchmove')
+  const event$ = O.merge(touchEnd$, touchStart$, touchMove$)
+    .map(target => ({target, event: 'preventDefault'}))
+  return {event$, touchStart$, touchEnd$, touchMove$, container$}
+}
+
+const model = ({completion$, touchStart$, touchEnd$, touchMove$, container$}) => {
+  const throttledMove$ = RAFThrottle(mux({start: touchStart$, end: touchEnd$, move: touchMove$}))
+  const width$ = container$.map(width).take(1)
+  const clientX = R.compose(R.prop('clientX'), R.head, R.prop('changedTouches'))
+  const transition$ = O.merge(touchStart$.map(false), touchEnd$.map(true)).startWith(true)
+  const seek$ = throttledMove$.map(clientX).withLatestFrom(width$, R.compose(R.clamp(0, 1), R.divide))
+  const translate$ = O.merge(completion$, seek$)
+    .distinctUntilChanged()
+    .map(completion => 100 * completion - 100)
+    .startWith(-100)
+    .map(x => x + '%')
+
+  return {seek$, transition$, translate$}
+}
+
+export default ({completion$, DOM}) => {
+  const {event$, touchStart$, touchEnd$, touchMove$, container$} = intent({DOM})
+  const vTree$ = view(model({completion$, touchStart$, touchEnd$, touchMove$, container$}))
   return {
-    DOM: vTree$
+    DOM: vTree$,
+    event$: event$
   }
 }
