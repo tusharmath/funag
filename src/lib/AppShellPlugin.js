eeb1b5f3dc78759b017182ac1b75f7f08ad42c29
Tushar Mathur <tusharmath@gmail.com>
2016-08-05 19:14:38 +0530

    refactor(appshell): remove rx intergration
    rx was making it difficult to understand what was going on inside

--- src/lib/AppShellPlugin.js
+++ src/lib/AppShellPlugin.js
@@ -4,25 +4,28 @@
 
 'use strict'
 
-import {Observable} from 'rx'
 import Cycle from '@cycle/core'
 import {name} from '../../package.json'
 import HTML from './../layouts/HTML'
+import * as R from 'ramda'
 
-const select = compiler => {
-  return {
-    events (event) {
-      return Observable.fromEventPattern(cb => compiler.plugin(event, (...i) => cb(i)))
-    }
-  }
-}
-
-const boilerplate = ({Main, bundle}) => sources => {
+export const wrapHTML = R.curry((bundle, __html) => HTML({
+  __html, __title: name, bundle
+}))
+export const boilerplate = R.curry((Main, bundle, sources) => {
   const main = Main(sources)
-  const vTree$ = main.DOM.first().map(__html => HTML({__html, __title: name, bundle}))
-  return Object.assign({}, main, {DOM: vTree$})
-}
-
+  const vTree$ = main.DOM.first().map(wrapHTML(bundle))
+  return R.merge(main, {DOM: vTree$})
+})
+export const getBundleName = ({outputOptions, hash}) => outputOptions.filename.replace('[hash]', hash)
+export const createAsset = (html) => ({
+  source: () => html,
+  size: () => html.length
+})
+export const onHTML = R.curry((compilation, cb, html) => {
+  compilation.assets['index.html'] = createAsset(html)
+  cb()
+})
 export class ApplicationShell {
   constructor ({Main, sources}) {
     this.Main = Main
@@ -31,20 +34,12 @@ export class ApplicationShell {
 
   apply (compiler) {
     const {Main, sources} = this
-    const emit$ = select(compiler).events('emit')
-    const bundle$ = emit$
-      .map(([{outputOptions, hash}]) => outputOptions.filename.replace('[hash]', hash))
-
-    emit$
-      .withLatestFrom(bundle$)
-      .flatMapLatest(([[com, cb], bundle]) => {
-        const html$ = Cycle.run(boilerplate({Main, bundle}), sources).sources.DOM
-        return html$.map(html => ({html, com, cb}))
-      })
-      .subscribe(({com, cb, html}) => {
-        com.assets['index.html'] = {source: () => html, size: () => html.length}
-        return cb()
-      })
+    const onEmit = (compilation, cb) => {
+      const bundle = getBundleName(compilation)
+      const html$ = Cycle.run(boilerplate(Main, bundle), sources).sources.DOM
+      return html$.subscribe(onHTML(compilation, cb))
+    }
+    compiler.plugin('emit', onEmit)
   }
 }
 
