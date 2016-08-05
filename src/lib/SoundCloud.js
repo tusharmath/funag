c943907e69003f6454ca7301483ee36708535f08
Tushar Mathur <tusharmath@gmail.com>
2016-08-05 20:33:57 +0530

    chore(deps): remove unused deps

--- src/lib/SoundCloud.js
+++ src/lib/SoundCloud.js
@@ -5,7 +5,6 @@
 'use strict'
 
 import qs from 'qs'
-import {partial} from 'funjector'
 
 const CLIENT_ID = '1862b9bf02ed7c80d0f545f835ad8773'
 
@@ -24,13 +23,6 @@ export const toURI = (path, params) => {
 export const get = (path, params) => {
   return fetch(toURI(path, params)).then(x => x.json())
 }
-
-// TODO: DEPRECATE
-export const searchTracks = partial(
-  (get, q$) => q$
-    .flatMapLatest(q => get('/tracks', {q}))
-    .share(),
-  get)
 
 export const durationFormat = time => {
   const mins = Math.floor(time / 60000)
--- test/test.SoundCloud.js
+++ test/test.SoundCloud.js
@@ -5,8 +5,7 @@
 'use strict'
 
 import test from 'ava'
-import {orig} from 'funjector'
-import {ReactiveTest, TestScheduler, Observable} from 'rx'
+import {ReactiveTest} from 'rx'
 import * as SC from '../src/lib/SoundCloud'
 const {onNext, onCompleted} = ReactiveTest
 
@@ -14,19 +13,4 @@ test('durationFormat()', t => {
   t.is(SC.durationFormat(5000), '0:50')
   t.is(SC.durationFormat(60000), '1:00')
   t.is(SC.durationFormat(645000), '10:45')
-})
-
-test('searchTracks()', t => {
-  const searchTracks = orig(SC.searchTracks)
-  const sh = new TestScheduler()
-
-  const get = (_, {q}) => Observable.just(q).delay(q * 100, sh)
-
-  const q$ = sh.createHotObservable(
-    onNext(210, 5),
-    onNext(220, 1)
-  )
-
-  const {messages} = sh.startScheduler(() => searchTracks(get, q$))
-  t.deepEqual(messages, [onNext(320, 1)])
 })
