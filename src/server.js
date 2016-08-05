a74eb368b4491d1587ee67430c568bcce564f1c3
Tushar Mathur <tusharmath@gmail.com>
2016-08-05 18:51:39 +0530

    chore(env): set default env to development

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@
 
 'use strict'
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
 const {forEach, map, curry, apply, values, compose, nthArg} = require('ramda')
 
 const httpProxy = require('http-proxy')
