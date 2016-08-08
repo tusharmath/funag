a416deb1e1fcc11076269034b3ee14b1b1e8cb62
Tushar Mathur <tusharmath@gmail.com>
2016-08-08 20:34:22 +0530

    refactor(themes): change prop names

--- src/lib/Themes.js
+++ src/lib/Themes.js
@@ -5,22 +5,23 @@
  * SAMPLE NAME : {BLOCK__ELEMENT__MODIFIER}
  * BLOCKS — fg: foreground color, bg: background color, br: border color, zDepth: shadow
  */
+
 export const Palette = {
   bg__artwork: '#F6F6F6',
   bg__artworkPlayingIcon: 'rgb(34, 34, 34)',
   bg__control: '#2196F3',
-  bg__header: '#2196F3',
+  bg__header: '#EEE',
   bg__scrobberIcon: '#2196F3',
-  bg__scrobberTrack: 'rgba(0, 0, 0, 0.3)',
+  bg__scrobberTrack: 'rgb(172, 172, 172)',
   bg__search: '#2196F3',
   br__playlistItem: '1px solid rgb(249, 246, 246)',
-  fg__artwork: 'rgba(0, 0, 0, 0.3)',
+  fg__artwork: 'rgb(172, 172, 172)',
+  fg__header: 'rgb(34, 34, 34)',
+  fg__header__light: 'rgb(172, 172, 172)',
   fg__playbackInfo__light: 'rgb(200, 218, 237)',
-  fg__playlistItem: 'rgb(34, 34, 34)',
+  fg__trackDetail: 'rgb(34, 34, 34)',
   fg__search: '#FFF',
-  fg__header: '#FFF',
-  fg__header__light: 'rgb(200, 218, 237)',
-  fg__trackDetail__light: 'rgb(200, 218, 237)',
+  fg__trackDetail__light: 'rgb(172, 172, 172)',
   zDepth__0: 'none !important',
   zDepth__1: '0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)',
   zDepth__2: '0 8px 17px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19)',
