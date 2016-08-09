a61c3a388adc609ac77fe0c8d21cea047f65ce6a
Tushar Mathur <tusharmath@gmail.com>
2016-08-09 18:51:12 +0530

    refactor(header): add header component

---
+++ src/components/header/header.style.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tushar.mathur on 08/08/16.
+ */
+
+'use strict'
+import {create} from '../../lib/CreateStyle'
+import {BlockSpace, Palette} from '../../lib/Themes'
+
+export default create({
+  headerContainer: {
+    paddingLeft: BlockSpace,
+    backgroundColor: Palette.bg__header,
+    boxShadow: Palette.zDepth__1,
+    color: Palette.fg__header,
+    transform: 'translateZ(0)',
+    '& small': {
+      color: Palette.fg__trackDetail__light,
+      fontSize: '0.6rem',
+      lineHeight: '1rem'
+    }
+  }
+})
