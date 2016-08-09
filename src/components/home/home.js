b20a054374d3a08f2a18834a28c5475df4d15f11
Tushar Mathur <tusharmath@gmail.com>
2016-08-09 18:59:37 +0530

    refactor(components): add home component

---
+++ src/components/home/home.js
@@ -0,0 +1,42 @@
+/**
+ * Created by tushar.mathur on 08/08/16.
+ */
+
+'use strict'
+
+import {Observable as O} from 'rx'
+import R from 'ramda'
+import {mux} from 'muxer'
+import * as SC from '../../lib/SoundCloud'
+import css from './home.style'
+import Header from '../header/header'
+import Playlist from '../playlist/playlist'
+
+const view = ({playlist, header}) => O
+  .combineLatest(header.DOM, playlist.DOM)
+  .map(views => <div className={css(css.home, 'flexCol')}>{views}</div>)
+
+const getAudioSink = selectedTrack$ => mux({
+  load: selectedTrack$
+    .map(SC.trackStreamURL)
+    .map(R.objOf('src'))
+})
+
+const createTrendingTracksRequest = () => O.just({
+  url: SC.toURI('/tracks'),
+  category: 'trending-tracks'
+})
+
+export default ({HTTP, AUDIO, DOM, ROUTER}) => {
+  const tracks$ = HTTP.select('trending-tracks').switch().pluck('body').share()
+  const header = Header({DOM, ROUTER})
+  const defaultTrack$ = tracks$.map(R.head)
+  const playlist = Playlist({tracks$, defaultTrack$, AUDIO, DOM})
+  const audioSink$ = getAudioSink(playlist.selectedTrack$)
+  return {
+    DOM: view({header, playlist}),
+    HTTP: createTrendingTracksRequest(),
+    AUDIO: O.merge(playlist.audio$, audioSink$),
+    ROUTER: O.merge(header.ROUTER)
+  }
+}
