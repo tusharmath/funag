29e1830544319a4237e86ae94b578d0940519770
Tushar Mathur <tusharmath@gmail.com>
2016-08-09 18:59:37 +0530

    refactor(components): add home component

---
+++ src/components/home/home.style.js
@@ -0,0 +1,13 @@
+/**
+ * Created by tushar.mathur on 08/08/16.
+ */
+
+'use strict'
+
+import {create} from '../../lib/CreateStyle'
+
+export default create({
+  home: {
+    height: '100%'
+  }
+})
