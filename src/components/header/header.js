efb00f9d516912b14ca4ef45eadb1f0acf002b44
Tushar Mathur <tusharmath@gmail.com>
2016-08-10 20:23:23 +0530

    refactor(header): add tabs

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,30 +4,37 @@
 
 'use strict'
 
-import {Observable as O} from 'rx'
 import css from './header.style'
 import IconButton from '../fa-icon-button/fa-icon-button'
+import Tab from '../tab/tab'
 
-const view = ({ROUTER}) => O.just(
-  <div className={css.headerContainer}>
-    <div className={css('rowMiddle')}>
-      <div className={css('flexSpread')}>
-        <div>
-          <strong>Funag</strong>
+const view = ({ROUTER, tab}) => tab
+  .DOM.map(tab =>
+    <div className={css(css.headerContainer, 'fade-in')}>
+      <div className={css('rowMiddle')}>
+        <div className={css('flexSpread', css.headerText)}>
+          <div>
+            <strong>Funag</strong>
+          </div>
+          <small>Unofficial soundcloud player</small>
         </div>
-        <small>Unofficial soundcloud player</small>
-      </div>
-      <div className={css('flexSpread')}>
-        <div className={css('rowRight')}>
-          {IconButton('Search', ROUTER.createHref('/search'))}
+        <div className={css('flexSpread')}>
+          <div className={css('rowRight')}>
+            {IconButton('Search', ROUTER.createHref('/search'))}
+            {IconButton('ellipsis-v', ROUTER.createHref('/search'))}
+          </div>
         </div>
       </div>
+      <div>{tab}</div>
     </div>
-  </div>
-)
+  )
 
 export default ({DOM, ROUTER}) => {
+  const nav = ['HISTORY', 'BOOKMARKED']
+  const sections = ['HISTORY CONTENT', 'BOOKMARKED CONTENT']
+  const tab = Tab({DOM, nav, sections})
+  tab.selectedTab$.subscribe(x => console.log('NAV', x))
   return {
-    DOM: view({ROUTER})
+    DOM: view({ROUTER, tab})
   }
 }
