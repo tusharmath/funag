3c99ded940b0291373b85e2b5f314dbf01340e67
Tushar Mathur <tusharmath@gmail.com>
2016-08-10 19:51:56 +0530

    refactor(DOMUtils): remove unused functions

--- src/lib/DOMUtils.js
+++ src/lib/DOMUtils.js
@@ -3,12 +3,9 @@
  */
 
 'use strict'
-import _ from 'ramda'
 import {Observable as O} from 'rx'
 import raf from 'raf'
 
-export const inputVal = $el => $el.events('keyup').map(x => x.target.value).distinctUntilChanged()
-export const action = _.curry((event, target) => ({target, event}))
 export const raf$ = () => {
   let started = false
   const update = cb => {
