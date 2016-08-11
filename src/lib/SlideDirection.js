9fef7c5486cbded019f26c55af99aa780eea9016
Tushar Mathur <tusharmath@gmail.com>
2016-08-11 17:38:56 +0530

    refactor(slide-direction): use touches to get touch event

--- src/lib/SlideDirection.js
+++ src/lib/SlideDirection.js
@@ -7,18 +7,17 @@
 import {Observable as O} from 'rx'
 import R from 'ramda'
 import sign from './Sign'
+import Touches from './Touches'
 
-const direction = x => Math.abs(x) > 0.5 ? R.negate(sign(x)) : 0
+const direction = R.curry((threshold, x) => Math.abs(x) > threshold ? R.negate(sign(x)) : 0)
 
 export default (el, maxWidth$, threshold = 0.5) => {
-  const start$ = el.events('touchstart')
-  const end$ = el.events('touchend')
-  return O.merge(start$, end$)
+  const {startX, endX} = Touches(el)
+  return O.merge(startX, endX)
     .bufferWithCount(2)
     .map(R.apply(R.subtract))
     .withLatestFrom(maxWidth$)
     .map(R.apply(R.divide))
-    .map(direction)
-    .tap(x => console.log(x))
-    .filter(x => x === 0)
+    .map(direction(threshold))
+    .filter(x => x !== 0)
 }
