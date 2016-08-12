a3826d370f776727f1c0bcc3aac9a73dbd629758
Tushar Mathur <tusharmath@gmail.com>
2016-08-12 20:28:05 +0530

    refactor(home): add section components

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,38 +6,43 @@
 
 import {Observable as O} from 'rx'
 import R from 'ramda'
-import {mux} from 'muxer'
+import color from 'randomcolor'
 import * as SC from '../../lib/SoundCloud'
 import css from './home.style'
 import Header from '../header/header'
 import Playlist from '../playlist/playlist'
-import HttpSelectBody from '../../lib/HttpSelectBody';
+import HttpSelectBody from '../../lib/HttpSelectBody'
+import MovableSection from '../movable-section/movable-section'
 
-const view = ({playlist, header}) => O
-  .combineLatest(header.DOM, playlist.DOM)
+const view = ({sections, header}) => O
+  .combineLatest(header.DOM, sections.DOM)
   .map(views => <div className={css(css.home, 'flexCol')}>{views}</div>)
-
-const getAudioSink = selectedTrack$ => mux({
-  load: selectedTrack$
-    .map(SC.trackStreamURL)
-    .map(R.objOf('src'))
-})
 
 const createTrendingTracksRequest = () => O.just({
   url: SC.toURI('/tracks', {}),
   category: 'trending-tracks'
 })
+const sectionContent = [
 
+  <div style={{backgroundColor: color(), padding: 50}}>A</div>,
+  <div style={{backgroundColor: color(), padding: 50}}>B</div>,
+  <div style={{backgroundColor: color(), padding: 50}}>C</div>,
+  <div style={{backgroundColor: color(), padding: 50}}>D</div>,
+  <div style={{backgroundColor: color(), padding: 50}}>E</div>,
+  <div style={{backgroundColor: color(), padding: 50}}>F</div>,
+  <div style={{backgroundColor: color(), padding: 50}}>G</div>
+]
 export default ({HTTP, AUDIO, DOM, ROUTER}) => {
+  const sections = MovableSection({DOM, sections: sectionContent})
   const tracks$ = HttpSelectBody(HTTP, 'trending-tracks')
   const header = Header({DOM, ROUTER})
   const defaultTrack$ = tracks$.map(R.head)
   const playlist = Playlist({tracks$, defaultTrack$, AUDIO, DOM})
-  const audioSink$ = getAudioSink(playlist.selectedTrack$)
+  playlist.selectedTrack$.subscribe(x => console.log('TODO: remove subscription', x))
   return {
-    DOM: view({header, playlist}),
+    DOM: view({header, sections}),
     HTTP: createTrendingTracksRequest(),
-    AUDIO: O.merge(playlist.audio$, audioSink$),
+    AUDIO: O.merge(playlist.audio$),
     ROUTER: O.merge(header.ROUTER)
   }
 }
