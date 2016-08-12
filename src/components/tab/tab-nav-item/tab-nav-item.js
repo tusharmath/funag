9e9de210fc9b1137a5a163bebd4edccd926475d8
Tushar Mathur <tusharmath@gmail.com>
2016-08-12 20:24:53 +0530

    refeactor(tab-nav-item): dimensions are calculated only once

--- src/components/tab/tab-nav-item/tab-nav-item.js
+++ src/components/tab/tab-nav-item/tab-nav-item.js
@@ -16,7 +16,10 @@ const TabNavItem = ({DOM, selected$, ROW, INDEX}) => {
   const title = ROW
   const li = DOM.select('li')
   const click$ = li.events('click').map(INDEX)
-  const bcr$ = getElementBCR(li.elements()).distinctUntilChanged()
+  const bcr$ = getElementBCR(li.elements())
+    .filter(dim => dim.height > 0 && dim.width > 0)
+    .take(1)
+    .shareReplay(1)
   return {DOM: view({title, selected$, INDEX}), click$, bcr$}
 }
 export default sources => isolate(TabNavItem)(sources)
