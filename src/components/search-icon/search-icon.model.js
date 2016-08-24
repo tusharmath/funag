05394e23e0f5f7051c8c863e242565e931f5b04f
Tushar Mathur <tusharmath@gmail.com>
2016-08-24 15:25:31 +0530

    refactor(search-icon): add getIconDOM() function

--- src/components/search-icon/search-icon.model.js
+++ src/components/search-icon/search-icon.model.js
@@ -5,6 +5,7 @@
 'use strict'
 
 import {Observable as O} from 'rx'
+import Loader from '../loader/loader'
 
 export const ICONS = {
   LOADER: 'loader',
@@ -13,10 +14,19 @@ export const ICONS = {
 }
 
 export const getIcon = (hasValue$, isLoading$) => {
-  return O.combineLatest(hasValue$.startWith(false), isLoading$.startWith(false))
+  return O.combineLatest(
+    hasValue$.startWith(false),
+    isLoading$.startWith(false)
+  )
     .map(([hasValue, isLoading]) => {
       if (isLoading) return ICONS.LOADER
       if (hasValue) return ICONS.CLEAR
       return ICONS.SEARCH
     })
 }
+
+export const getIconDOM = (icon) => ({
+  [ICONS.LOADER]: Loader(),
+  [ICONS.CLEAR]: <x-icon-button key='close' attrs-icon='close'/>,
+  [ICONS.SEARCH]: <x-icon-button key='search' attrs-icon='search'/>
+})[icon]
