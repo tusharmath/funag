4da94247c2006cff2eb80ef2d39996a05ae33a3c
Tushar Mathur <tusharmath@gmail.com>
2016-08-26 00:08:04 +0530

    refactor(controls): split code into smaller files

--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -6,33 +6,8 @@
 import {Observable as O} from 'rx'
 import Scrobber from '../scrobber/scrobber'
 import Playback from '../playback/playback'
-import css from './controls.style'
-
-const view = ({playback, scrobber, show$}) => {
-  return O
-    .combineLatest(scrobber.DOM, playback.DOM, show$)
-    .map(([scrobber, playback, show, height]) =>
-      <div class={{[css.hide]: !show}}
-           className={css(css.controlsContainer, 'controls')}>
-        {scrobber}
-        {playback}
-      </div>
-    )
-}
-
-const screenReduced = (EVENTS) => {
-  const windowHeight$ = EVENTS.select('resize').pluck('innerHeight')
-  return windowHeight$.withLatestFrom(windowHeight$.first())
-    .map(([final, initial]) => final > 0.9 * initial).startWith(true)
-}
-
-const completion = ({AUDIO, STORE}) => {
-  return O.merge(
-    AUDIO.events('timeUpdate').map(x => x.currentTime / x.duration),
-    AUDIO.events('ended').map(1),
-    STORE.select('track.selected').filter(Boolean).map(0)
-  )
-}
+import view from './controls.view'
+import {completion, screenReduced} from './controls.model'
 
 const model = ({AUDIO, STORE, EVENTS}) => {
   const completion$ = completion({AUDIO, STORE})
