2a133fd70b9f5b3e90ad590662843f5fc90d6f8f
Tushar Mathur <tusharmath@gmail.com>
2016-08-27 14:31:31 +0530

    refactor(header): remove translateZ(0)

--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -12,7 +12,6 @@ export default addRules({
   headerContainer: {
     backgroundColor: Palette.bg__header,
     color: Palette.fg__header,
-    transform: 'translateZ(0)',
     '& small': {
       color: Palette.fg__trackDetail__light,
       fontSize: '0.6rem',
