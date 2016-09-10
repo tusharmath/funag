d58c4221599ea7b5ffe956899ecb9916ec083b7d
Tushar Mathur <tusharmath@gmail.com>
2016-09-10 12:46:25 +0530

    refactor(http): add debounce support

--- src/web-components/http/http.js
+++ src/web-components/http/http.js
@@ -11,23 +11,41 @@ import {Request, Response} from './http.events'
 
 export default class Http extends HTMLElement {
   createdCallback () {
+    this.__timeout = null
+    this.debounce = parseInt(this.getAttribute('debounce')) || 0
     this.onRequest = this.onRequest.bind(this)
     this.__root = getRootNode(this)
     document.addEventListener('http-request', this.onRequest)
+  }
+
+  set debounce (value) {
+    this.__debounce = value
+  }
+
+  get debounce () {
+    return this.__debounce
   }
 
   __dispatch (response) {
     this.dispatchEvent(Response.of(response))
   }
 
+  makeRequest ({url, params}) {
+    fetch(url, params)
+      .then(response => response.json())
+      .then(
+        json => this.__dispatch(json),
+        function (err) { throw err }
+      )
+  }
+
   onRequest (ev) {
     if ((ev instanceof Request)) {
-      fetch(ev.url, ev.params)
-        .then(response => response.json())
-        .then(
-          json => this.__dispatch(json),
-          function (err) { throw err }
-        )
+      if (this.__timeout) clearTimeout(this.__timeout)
+      this.__timeout = setTimeout(() => {
+        this.__timeout = null
+        this.makeRequest(ev)
+      }, this.debounce)
     }
   }
 }
