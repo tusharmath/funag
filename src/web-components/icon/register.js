fb123bb2fb496b20c7f26e526e1a566bfbbe9866
Tushar Mathur <tusharmath@gmail.com>
2016-09-11 21:19:56 +0530

    fix(icon): remove from gitignore

---
+++ src/web-components/icon/register.js
@@ -0,0 +1,26 @@
+/**
+ * Created by tushar.mathur on 08/09/16.
+ */
+
+'use strict'
+
+import h from 'hyperscript'
+import rwc from 'rwc'
+import registerWC from '../../lib/registerWC'
+import FunagIcon from './icon'
+import snabbdomPatcher from '../../lib/snabbdom-patcher'
+
+const CSS = '@import "//fonts.googleapis.com/icon?family=Material+Icons";'
+document.querySelector('head').appendChild(h('link', {
+  attrs: {
+    rel: 'stylesheet',
+    type: 'text/css',
+    href: '//fonts.googleapis.com/icon?family=Material+Icons'
+  }
+}))
+
+registerWC('fg-icon', rwc.createWCProto(root => {
+  root.appendChild(h('style', CSS))
+  return snabbdomPatcher(root)
+}, FunagIcon))
+
