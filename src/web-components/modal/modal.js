9ece1da29a627c67a0049e238e4399f6d55fad0b
Tushar Mathur <tusharmath@gmail.com>
2016-09-12 18:19:58 +0530

    refactor(modal): remove animations

--- src/web-components/modal/modal.js
+++ src/web-components/modal/modal.js
@@ -9,46 +9,26 @@ import R from 'ramda'
 
 export default {
   props: ['show'],
-  init () {
-    return {
-      show: true,
-      showContainer: true,
-      showSlot: true
-    }
-  },
+  init () { return {show: false} },
   update (state, {type, params}) {
     switch (type) {
       case '@@rwc/prop/show':
         return R.assoc('show', params, state)
       case 'OVERLAY_CLICK':
         return R.assoc('show', false, state)
-      case 'CONTAINER_TRANSITION_ENDED':
-        return R.assoc('showContainer', false, state)
-      case 'SLOT_TRANSITION_ENDED':
-        return R.assoc('showSlot', false, state)
       default:
         return state
     }
   },
-  view ({show, showSlot, showContainer}, dispatch) {
-    const isHidden = showContainer === false && showSlot === false
+  view ({show}, dispatch) {
     return h('div', [
-      isHidden ? '' : h('div.modal-container', {
-        class: {hidden: !show},
-        on: {
-          transitionend: dispatch('CONTAINER_TRANSITION_ENDED'),
-          click: dispatch('OVERLAY_CLICK', {preventDefault: true})
-        }
+      !show ? '' : h('div.modal-container', {
+        on: {click: dispatch('OVERLAY_CLICK', {preventDefault: true})}
       }, [
         h('fg-disable-scroll'),
-        h('div.slot-container', {
-          on: {
-            transitionend: dispatch('SLOT_TRANSITION_ENDED'),
-            touchstart: dispatch('touchStart', {preventDefault: true}),
-            touchmove: dispatch('touchMove', {preventDefault: true}),
-            touchend: dispatch('touchEnd', {preventDefault: true})
-          }
-        }, [h('slot')])
+        h('div.slot-container', [
+          h('slot')
+        ])
       ])
     ])
   }
--- src/web-components/modal/modal.style.js
+++ src/web-components/modal/modal.style.js
@@ -5,11 +5,13 @@
 'use strict'
 
 import {createStyleSheet} from '../../lib/JSSHelpers'
+import {BlockSpace} from '../../lib/Themes'
 export default createStyleSheet({
   '.modal-container': {
     position: 'fixed',
     width: '100%',
     top: 0,
+    left: 0,
     zIndex: 1,
     display: 'flex',
     flexDirection: 'column-reverse',
@@ -19,6 +21,7 @@ export default createStyleSheet({
   },
   '.slot-container': {
     backgroundColor: '#FFF',
+    padding: BlockSpace,
     boxShadow: '0px -4px 4px 0px rgba(0, 0, 0, 0.25)',
     transition: 'all 300ms ease-in'
   },
