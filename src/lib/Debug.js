417d4abdb6661ae62a570bacb4c4a95752f571fc
Tushar Mathur <tusharmath@gmail.com>
2016-09-16 11:36:05 +0530

    refactor(debug): check for dev env

--- src/lib/Debug.js
+++ src/lib/Debug.js
@@ -5,7 +5,7 @@
 'use strict'
 
 const noop = function () {}
-const Debug = APP_CONFIG === 'production' ? noop : function (callback) {
+const Debug = APP_CONFIG.env === 'development' ? function (callback) {
   callback()
-}
+} : noop
 export default Debug
