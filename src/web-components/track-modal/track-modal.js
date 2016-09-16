16ec75de5f47607c587564cf5d1f8f713dbfb7de
Tushar Mathur <tusharmath@gmail.com>
2016-09-16 11:01:53 +0530

    chore(track-list): revert abf399f2bb0f09da2185340f66007acdd30ea5c3

---
+++ src/web-components/track-modal/track-modal.js
@@ -0,0 +1,46 @@
+/**
+ * Created by tushar.mathur on 12/09/16.
+ */
+
+'use strict'
+
+import h from 'snabbdom/h'
+import R from 'ramda'
+import Value from '../../lib/value'
+
+export default {
+  props: ['show', 'track'],
+  init () {
+    return {
+      show: false,
+      track: null
+    }
+  },
+  update (state, {params, type}) {
+    switch (type) {
+      case '@@rwc/prop/show':
+        return R.assoc('show', Value.get(params), state)
+      case '@@rwc/prop/track':
+        return R.assoc('track', params, state)
+      default:
+        return state
+    }
+  },
+  view ({show, track, menu}) {
+    if (!track) return ''
+    return h(`fg-modal`, {props: {show: Value.of(show)}}, [
+      h(`div.trackContainer`, [
+        h('fg-track-artwork', {
+          props: {track, selected: false, playing: false}
+        }),
+        h(`div.trackDetail`, [
+          h(`div.title`, [track.title]),
+          h(`div.artist`, [track.user.username])
+        ])
+      ]),
+      h('div.menu', [
+        h(`slot`)
+      ])
+    ])
+  }
+}
