5797e7130e4874bb34dd980c75e43ed3c72ba0ec
Tushar Mathur <tusharmath@gmail.com>
2016-09-19 20:53:44 +0530

    refactor(draggable): remove handlers after getting detached

--- src/web-components/draggable/draggable.js
+++ src/web-components/draggable/draggable.js
@@ -20,25 +20,14 @@ export default {
   createdCallback () {
     this.__dispatchTouchEvent = this.__dispatchTouchEvent.bind(this)
     this.__setChangedTouches = this.__setChangedTouches.bind(this)
+    this.__setChangedTouches = this.__setChangedTouches.bind(this)
+    this.__onTouchStart = this.__onTouchStart.bind(this)
+    this.__onTouchEnd = this.__onTouchEnd.bind(this)
 
     // TODO: Lazy attach
     this.addEventListener('touchmove', this.__setChangedTouches)
-    this.addEventListener('touchstart', (ev) => {
-      ev.preventDefault()
-      ev.stopPropagation()
-      this.__clientX = 0
-      this.__clientY = 0
-      this.__dimensions = this.getBoundingClientRect()
-      this.__setChangedTouches(ev)
-      this.__queue = raf.start(this.__dispatchTouchEvent)
-    })
-    this.addEventListener('touchend', () => {
-      this.__clientX = 0
-      this.__clientY = 0
-      this.__dispatchTouchEvent()
-      this.__lastEV = null
-      raf.stop(this.__queue)
-    })
+    this.addEventListener('touchstart', this.__onTouchStart)
+    this.addEventListener('touchend', this.__onTouchEnd)
   },
 
   __dispatchTouchEvent () {
@@ -46,6 +35,24 @@ export default {
       dragX: this.__clientX / this.__dimensions.width,
       dragY: this.__clientY / this.__dimensions.height
     }))
+  },
+
+  __onTouchStart (ev) {
+    ev.preventDefault()
+    ev.stopPropagation()
+    this.__clientX = 0
+    this.__clientY = 0
+    this.__dimensions = this.getBoundingClientRect()
+    this.__setChangedTouches(ev)
+    this.__queue = raf.start(this.__dispatchTouchEvent)
+  },
+
+  __onTouchEnd () {
+    this.__clientX = 0
+    this.__clientY = 0
+    this.__dispatchTouchEvent()
+    this.__lastEV = null
+    raf.stop(this.__queue)
   },
 
   __setChangedTouches (ev) {
@@ -56,5 +63,11 @@ export default {
       this.__clientY = this.__clientY + deltaY(ev, this.__lastEV)
     }
     this.__lastEV = ev
+  },
+
+  detachedCallback () {
+    this.removeEventListener('touchmove', this.__setChangedTouches)
+    this.removeEventListener('touchstart', this.__onTouchStart)
+    this.removeEventListener('touchend', this.__onTouchEnd)
   }
 }
